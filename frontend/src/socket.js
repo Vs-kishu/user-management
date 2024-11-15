ff2a94213f9eff481944c1ff653ec8731f1890ed
Krishna Kant Verma <kishanverma1208143@gmail.com>
2024-11-15 18:33:51 +0530

    bug

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -4,7 +4,7 @@ const userId = JSON.parse(localStorage.getItem("userToken"))?.userId;
 
 const socket = io("https://user-management-flax-three.vercel.app", {
   query: { userId: userId },
-  transports: ['websocket'], // Force WebSocket transport
+ transports: ["polling", "websocket"], // Force WebSocket transport
   reconnectionAttempts: 5,   // Try to reconnect 5 times
   reconnectionDelay: 1000,   // Wait 1 second between reconnection attempts
   timeout: 20000            // Increase timeout to 20 seconds
